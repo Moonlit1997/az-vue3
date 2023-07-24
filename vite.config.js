c419260371055309eec493cde6d6236af0079040
Moonlit1997 <zhouxiyu1997@gmail.com>
2023-07-24 17:04:35 +0800

    添加了打包出问题

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 引入@vitejs/plugin-legacy
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [legacy({
+  targets: ['defaults', 'not IE 11']
+}),vue()],base: './',
 })

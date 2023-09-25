3b21210312cbf429a9b42ffd4b94c5ca30ea5a7b
Moonlit1997 <zhouxiyu1997@gmail.com>
2023-09-25 19:31:01 +0800

    模板

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,58 @@
 </script>
 
 <template>
+  <div class="relative h-screen">
+    <img
+      src="./assets/home.png"
+      alt="全覆盖图片"
+      class="absolute h-full w-full object-cover"
+      v-show="IsImages"
+    />
+    <img
+      src="./assets/home2.png"
+      alt="全覆盖图片"
+      class="absolute h-full w-full object-cover"
+      v-show="!IsImages"
+    />
+
+    <!-- 遮罩层 -->
+    <div class="absolute"></div>
+    <!-- 三栏布局 -->
+    <div class="flex h-full text-green-700">
+      <!-- 左栏 -->
+      <div class="z-40 flex-1" @click="change"></div>
+
+      <!-- 中栏 -->
+      <div class="flex-2 w-[480px]"></div>
+
+      <!-- 右栏 -->
+      <div class="z-40 flex-1" @click="change"></div>
+    </div>
+  </div>
+
   <!-- <HelloWorld msg="Vite + Vue" /> -->
 
-  <RouterView />
+  <!-- <RouterView /> -->
   <!-- <RouterLink to="/">Home</RouterLink> |
   <RouterLink to="/about">About</RouterLink> |
   <RouterLink to="/side">Side</RouterLink>|
   <RouterLink to="/user/111">User</RouterLink>| -->
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      IsImages: true,
+    };
+  },
+  methods: {
+    change() {
+      console.log("change");
+      this.IsImages = !this.IsImages;
+      // this.$router.push({ path: "/about" });
+      //改变当前图片images2的地址
+    },
+  },
+};
+</script>
 <style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="relative">
-    <img src="./../assets/home.png" alt="" class="images2" @click="change" />
+  <div class="relative h-screen">
+    <img
+      src="./../assets/home.png"
+      alt="全覆盖图片"
+      class="absolute h-full w-full object-cover"
+      v-show="IsImages"
+    />
+    <img
+      src="./../assets/home2.png"
+      alt="全覆盖图片"
+      class="absolute h-full w-full object-cover"
+      v-show="!IsImages"
+    />
 
-    <div class="images2 absolute z-10 flex h-screen w-screen">
-      <div class="z-10 flex-1 bg-white">222</div>
-      <div class="bg-white">222</div>
+    <!-- 遮罩层 -->
+    <div class="absolute inset-0 opacity-50"></div>
+
+    <!-- 三栏布局 -->
+    <div class="z-10 flex h-full">
+      <!-- 左栏 -->
+      <div class="b flex-1" @click="change">左栏内容</div>
+
+      <!-- 中栏 -->
+      <div class="flex-2 w-[480px]">中栏内容</div>
+
+      <!-- 右栏 -->
+      <div class="flex-1" @click="change">右栏内容</div>
     </div>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      IsImages: true,
+    };
+  },
   methods: {
     change() {
-      this.$router.push({ path: "/about" });
+      console.log("change");
+      this.IsImages = !this.IsImages;
+      // this.$router.push({ path: "/about" });
       //改变当前图片images2的地址
     },
   },
@@ -26,6 +54,8 @@ export default {
   height: 100%;
   object-fit: cover;
   object-position: center;
-  src: url("./../assets/home.png");
+  background-image: url("./../assets/home.png");
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
